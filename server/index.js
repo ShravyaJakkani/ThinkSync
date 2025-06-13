b978d3f24be393d6ac8cc9c77455a305b9975d6e
ShravyaJakkani <jakkanishravya@gmail.com>
2025-06-13 16:07:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -80,3 +80,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+ const innovationRoutes = require('./routes/innovationRoutes');
+ app.use('/api/innovation', innovationRoutes);
+
