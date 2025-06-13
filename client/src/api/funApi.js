079210612583b0bf764d2ae0577f2a2cbb6c5316
ShravyaJakkani <jakkanishravya@gmail.com>
2025-06-13 14:00:38 +0530

    Update funApi.js

--- client/src/api/funApi.js
+++ client/src/api/funApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://thinksync-backend.onrender.com";
+const API_URL = "https://thinksync-backend.onrender.com/api/fun";
 
 export const fetchFunPosts = async () => {
   const res = await axios.get(API_URL);
