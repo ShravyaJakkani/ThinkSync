b702e711e926da1a51512f599454d3d7f81cb812
ShravyaJakkani <jakkanishravya@gmail.com>
2025-06-13 16:12:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const path = require("path");
-// const cors = require('cors');
-// require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require("path");
+const cors = require('cors');
+require('dotenv').config();
 
-// const app = express();
-// const PORT = process.env.PORT || 5000;
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-// const allowedOrigins = [
-//   'http://localhost:5173',
-//   'https://thinksync-frontend.onrender.com'
-// ];
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://thinksync-frontend.onrender.com'
+];
 
 // app.use(cors({
 //   origin: function (origin, callback) {
@@ -26,60 +26,16 @@
 
 // app.options('*', cors()); // Pre-flight handling
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-// app.get("/", (req, res) => {
-//   res.send("Backend running!");
-// });
-
-
-// // Routes
-// // const innovationRoutes = require('./routes/innovationRoutes');
-// // app.use('/api/innovation', innovationRoutes);
-
-// // const poetryRoutes = require('./routes/poetryRoutes');
-// // app.use('/api/poetry', poetryRoutes);
-
-// // const questionPaperRoutes = require("./routes/questionPaperRoutes");
-// // app.use("/api/questionpapers", questionPaperRoutes);
-
-// // const funRoutes = require('./routes/funRoutes');
-// // app.use("/api/fun", funRoutes);
-
-// // const artRoutes = require('./routes/artRoutes');
-// // app.use("/api/art", artRoutes);
-
-// // const announcementRoutes = require("./routes/announcementRoutes");
-// // app.use("/api/announcement", announcementRoutes);
-
-// // const achievementRoutes = require("./routes/achievementRoutes");
-// // app.use("/api/achievement", achievementRoutes);
-
-// // const opportunityRoutes = require("./routes/opportunityRoutes");
-// // app.use("/api/opportunity", opportunityRoutes);
-
-
-
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// }).then(() => {
-//   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// }).catch(err => console.error('MongoDB connection error:', err));
-
-const express = require("express");
-const app = express();
-
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.get("/", (req, res) => {
-  res.send("Minimal server running fine!");
+  res.send("Backend running!");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+
 const innovationRoutes = require('./routes/innovationRoutes');
 app.use('/api/innovation', innovationRoutes);
 
@@ -103,3 +59,47 @@ app.use("/api/achievement", achievementRoutes);
 
 const opportunityRoutes = require("./routes/opportunityRoutes");
 app.use("/api/opportunity", opportunityRoutes);
+
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}).catch(err => console.error('MongoDB connection error:', err));
+
+// const express = require("express");
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Minimal server running fine!");
+// });
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+// const innovationRoutes = require('./routes/innovationRoutes');
+// app.use('/api/innovation', innovationRoutes);
+
+// const poetryRoutes = require('./routes/poetryRoutes');
+// app.use('/api/poetry', poetryRoutes);
+
+// const questionPaperRoutes = require("./routes/questionPaperRoutes");
+// app.use("/api/questionpapers", questionPaperRoutes);
+
+// const funRoutes = require('./routes/funRoutes');
+// app.use("/api/fun", funRoutes);
+
+// const artRoutes = require('./routes/artRoutes');
+// app.use("/api/art", artRoutes);
+
+// const announcementRoutes = require("./routes/announcementRoutes");
+// app.use("/api/announcement", announcementRoutes);
+
+// const achievementRoutes = require("./routes/achievementRoutes");
+// app.use("/api/achievement", achievementRoutes);
+
+// const opportunityRoutes = require("./routes/opportunityRoutes");
+// app.use("/api/opportunity", opportunityRoutes);
