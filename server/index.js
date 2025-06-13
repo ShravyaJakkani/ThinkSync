ab4f0a9942ea2e47d2da5344e62c3ceb46e0c239
ShravyaJakkani <jakkanishravya@gmail.com>
2025-06-13 14:06:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,22 +7,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://thinksync-frontend.onrender.com'  
-];
-
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*', // Temporarily allow all origins (not recommended for production)
   methods: ['GET', 'POST', 'DELETE'],
-  credentials: true,
 };
+
 app.use(cors(corsOptions));
 
 app.use(express.json());
