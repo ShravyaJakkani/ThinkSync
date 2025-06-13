d8f83cafb0bdce2c6ea15a8caeaaba671eb02eed
ShravyaJakkani <jakkanishravya@gmail.com>
2025-06-13 13:58:38 +0530

    Update innovationApi.js

--- client/src/api/innovationApi.js
+++ client/src/api/innovationApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://thinksync-backend.onrender.com';
+const API_URL = 'https://thinksync-backend.onrender.com/api/innovation';
 
 export const fetchInnovationPosts = async () => {
   const response = await axios.get(API_URL);
