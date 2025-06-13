5b7f9e0b845ddbe2c67ab62f08d545d94a9b7f41
ShravyaJakkani <jakkanishravya@gmail.com>
2025-06-13 14:02:06 +0530

    Update questionPapersApi.js

--- client/src/api/questionPapersApi.js
+++ client/src/api/questionPapersApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://thinksync-backend.onrender.com";
+const API_URL = "https://thinksync-backend.onrender.com/api/questionpapers";
 
 export const fetchQuestionPapers = async () => {
   const res = await axios.get(API_URL);
