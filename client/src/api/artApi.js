444bd5b6adcb51bd4c54196ec70c0c98a057ae94
ShravyaJakkani <jakkanishravya@gmail.com>
2025-06-13 14:00:08 +0530

    Update artApi.js

--- client/src/api/artApi.js
+++ client/src/api/artApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://thinksync-backend.onrender.com';
+const BASE_URL = 'https://thinksync-backend.onrender.com/api/art';
 
 export const fetchArtPosts = async () => {
   const res = await axios.get(BASE_URL);
