07c362e36361a54f5956de1dc613042efbf9eaa0
ShravyaJakkani <jakkanishravya@gmail.com>
2025-06-17 14:21:39 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,20 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';  // ✅ Import
+// import App from './App';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';  // ✅ Import
 import App from './App';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <App />
 );
+
 
