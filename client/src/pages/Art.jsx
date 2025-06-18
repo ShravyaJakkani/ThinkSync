9bf62f8a724dbe81984d7938e21a975c86174d0f
ShravyaJakkani <jakkanishravya@gmail.com>
2025-06-18 11:08:32 +0530

    Update Art.jsx

--- client/src/pages/Art.jsx
+++ client/src/pages/Art.jsx
@@ -69,7 +69,7 @@ const handleLike = async (postId) => {
               <p className="text-sm text-gray-600 mb-1">Posted by: <strong>{post.userId}</strong></p> 
               {post.image && (
                 <img
-                  src={`http://localhost:5000${post.image}`}
+                  src={post.image}
                   style={{ maxWidth: '100%', maxHeight: '200px', objectFit: 'cover' }}
                 />
               )}
