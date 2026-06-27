e7a2c2eeb6b261b7784c578e78ca066f0b00671a
ShravyaJakkani <jakkanishravya@gmail.com>
2026-06-27 07:38:19 +0530

    Update poetryApi.js

--- client/src/api/poetryApi.js
+++ client/src/api/poetryApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const BASE_URL = 'https://thinksync-backend.onrender.com/api/poetry';
-const API_URL='http://127.0.0.1:5050/api/poetry';
+const API_URL='https://thinksync-backend.onrender.com/api/poetry';
 
 export const fetchPoetryPosts = async () => {
   const res = await axios.get(API_URL);
